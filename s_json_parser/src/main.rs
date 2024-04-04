3b94789f83fe609b3fbd40557f37799d6080e9ac
charlie.cm.choi <charlie.cm.choi@openedges.com>
2024-04-04 15:06:35 +0900

    refactoring code

--- s_json_parser/src/main.rs
+++ s_json_parser/src/main.rs
@@ -8,14 +8,18 @@ mod r#struct;
 
 // Json 을 struct 로 converting 해보자
 fn main() -> Result<(), io::Error> {
+    parse_test()?;
+
+    Ok(())
+}
+
+fn parse_test() -> Result<Vec<User>, io::Error> {
     let mut jsonStr = String::new();
     File::open("s_json_parser/data/parsor_test.json")?
         .read_to_string(&mut jsonStr)?
         .to_string();
 
-    let convertedJson: Vec<User> = serde_json::from_str(&jsonStr)?;
+    let converted_json: Vec<User> = serde_json::from_str(&jsonStr)?;
 
-    println!("{:?}", convertedJson);
-
-    Ok(())
+    Ok(converted_json)
 }
