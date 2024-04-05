693aae94ecaa94e6100d2d1b1e2aa99aff0d8a34
charlie.cm.choi <charlie.cm.choi@openedges.com>
2024-04-05 11:19:52 +0900

    rename variable name

--- s_json_parser/src/main.rs
+++ s_json_parser/src/main.rs
@@ -9,34 +9,34 @@ mod r#struct;
 
 // Json 을 struct 로 converting 해보자
 fn main() -> Result<(), io::Error> {
-    let pu = parse_test(String::from("s_json_parser/data/parser_test.json").as_str())?;
-    let njpu = nested_json_parse_test(
+    let _pu = parse_test(String::from("s_json_parser/data/parser_test.json").as_str())?;
+    let _njpu = nested_json_parse_test(
         String::from("s_json_parser/data/nested_parser_test.json").as_str(),
     )?;
 
-    assert_eq!(pu.len(), njpu.len());
+    assert_eq!(_pu.len(), _njpu.len());
 
     Ok(())
 }
 
 fn parse_test(path: &str) -> Result<Vec<User>, io::Error> {
-    let jsonStr = read_file(path)?;
-    let converted_json: Vec<User> = serde_json::from_str(&jsonStr)?;
+    let json_str = read_file(path)?;
+    let converted_json: Vec<User> = serde_json::from_str(&json_str)?;
 
     Ok(converted_json)
 }
 
 fn nested_json_parse_test(path: &str) -> Result<Vec<User>, io::Error> {
-    let jsonStr = read_file(path)?;
-    let converted_json: Value = serde_json::from_str(&jsonStr)?;
+    let json_str = read_file(path)?;
+    let converted_json: Value = serde_json::from_str(&json_str)?;
 
-    let userVec: Vec<User> = Vec::new();
+    let user_vec: Vec<User> = Vec::new();
 
-    Ok(userVec)
+    Ok(user_vec)
 }
 
 fn read_file(path: &str) -> Result<String, io::Error> {
-    let mut jsonStr = String::new();
-    File::open(path)?.read_to_string(&mut jsonStr)?.to_string();
-    Ok(jsonStr)
+    let mut json_str = String::new();
+    File::open(path)?.read_to_string(&mut json_str)?.to_string();
+    Ok(json_str)
 }
