f3e990fca417db2ee33df0307e7edcae63f0c801
charlie.cm.choi <charlie.cm.choi@openedges.com>
2024-06-02 16:01:15 +0900

    name 설정

--- s_chat/server/src/main.rs
+++ s_chat/server/src/main.rs
@@ -1,6 +1,7 @@
+use std::collections::HashMap;
 use std::error::Error;
-use std::io::BufWriter;
-use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
+
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
 // Rust Tokio Crate 이용 socket server 구현
@@ -8,6 +9,9 @@ use tokio::net::{TcpListener, TcpStream};
 async fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
     println!("Start Server -----");
+
+    // TODO
+    let mut room_name_map: Box<HashMap<&str, &str>> = Box::new(HashMap::new());
 
     loop {
         let (stream, _) = listener.accept().await?;
@@ -18,6 +22,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 async fn process(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    set_room(&mut stream).await.unwrap();
     loop {
         let mut buf = [0; 1024];
         let n = match stream.read(&mut buf).await {
@@ -36,8 +41,20 @@ async fn process(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
             eprintln!("failed to write to socket; err = {:?}", e);
             return Ok(());
         } else {
-            let send_message = format!("Response message: {}\n", message);
-            stream.write_all(send_message.as_bytes()).await.unwrap();
+            // let send_message = format!("Response message: {}\n", message);
+            // stream.write_all(send_message.as_bytes()).await.unwrap();
         }
     }
+}
+
+async fn set_room(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
+    let send_message = "Set your name: \n".to_string();
+    stream.write_all(send_message.as_bytes()).await.unwrap();
+
+    let mut buf = [0; 1024];
+    let _ = stream.read(&mut buf).await.unwrap();
+    let buf_str = String::from_utf8(Vec::from(buf))?;
+    let name = buf_str.split('\n').next().unwrap();
+
+    Ok(())
 }
