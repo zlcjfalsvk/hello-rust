a54bf0aa17bacac5bf2c94a95347fe359c07931e
charlie.cm.choi <charlie.cm.choi@openedges.com>
2024-06-24 15:32:01 +0900

    s_crypto 코드 작성

--- s_crypto/src/main.rs
+++ s_crypto/src/main.rs
@@ -1,14 +1,15 @@
+use base64::{engine::general_purpose, Engine as _};
 use chacha20poly1305::{
     aead::{Aead, AeadCore, KeyInit, OsRng},
-    ChaCha20Poly1305,
+    ChaCha20Poly1305, Key,
 };
 use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
 
 fn main() {
-    println!("start asymmetric_encryption_rsa");
-    asymmetric_encryption_rsa();
-    println!("-------------------------------");
-    println!("start symmetric_encryption_chacha20");
+    // println!("start asymmetric_encryption_rsa");
+    // asymmetric_encryption_rsa();
+    // println!("-------------------------------");
+    // println!("start symmetric_encryption_chacha20");
     symmetric_encryption_chacha20().unwrap();
 }
 
@@ -42,6 +43,17 @@ fn asymmetric_encryption_rsa() {
 
 fn symmetric_encryption_chacha20() -> Result<(), chacha20poly1305::Error> {
     let key = ChaCha20Poly1305::generate_key(&mut OsRng);
+    let key_base64 = general_purpose::STANDARD.encode(&key);
+    println!("Key: {:?}", key);
+    println!("Base64 Encoded Key: {}", key_base64);
+
+    let decoded_key = general_purpose::STANDARD
+        .decode(&key_base64)
+        .expect("Failed to decode Base64 key");
+    let key = Key::from_slice(&decoded_key);
+
+    println!("Base64 Decoded Key: {:?}", key);
+
     let cipher = ChaCha20Poly1305::new(&key);
     let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng); // 96-bits; unique per message
 
