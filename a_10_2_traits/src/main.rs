70a5352a9288edcd8df5b2e9c2034efae5e428da
charlie.cm.choi <charlie.cm.choi@openedges.com>
2024-06-25 23:07:04 +0900

    a_10_2

--- a_10_2_traits/src/main.rs
+++ a_10_2_traits/src/main.rs
@@ -70,12 +70,33 @@ where
 
 // trait 를 구현하는 타입 반환
 // 클로저 및 반복자의 컨텍스트에서 굉장히 유용
-fn returns_summarizable() -> impl Summary {
-    Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
+// fn returns_summarizable() -> impl Summary {
+//     Tweet {
+//         username: String::from("horse_ebooks"),
+//         content: String::from("of course, as you probably already know, people"),
+//         reply: false,
+//         retweet: false,
+//     }
+// }
+
+fn returns_summarizable(switch: bool) -> Box<dyn Summary> {
+    if switch {
+        Box::new(NewsArticle {
+            headline: String::from("Penguins win the Stanley Cup Championship!"),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best \
+                 hockey team in the NHL.",
+            ),
+        })
+    } else {
+        Box::new(Tweet {
+            username: String::from("horse_ebooks"),
+            content: String::from("of course, as you probably already know, people"),
+            reply: false,
+            retweet: false,
+        })
     }
 }
 
